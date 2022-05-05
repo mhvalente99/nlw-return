45b66802a3a2756457a691b909bdc4fde3a5eea3
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-05-04 21:12:15 -0300

    feat: server - separate route in other file without SOLID

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,12 @@
 import express from "express";
-import nodemailer from "nodemailer";
 
-import { prisma } from "./prisma";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "0bf0fb448a423d",
-    pass: "b91ffa627a5576",
-  },
-});
-
-app.post("/feedbacks", async (request, response) => {
-  const { type, comment, screenshot } = request.body;
-
-  await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
-  });
-
-  await transport.sendMail({
-    from: "Equipe Feedget <oi@feedget.com>",
-    to: "Matheus Valente <matheushenrique4@hotmail.com>",
-    subject: "Novo feedback",
-    html: [`<p>Tipo do feedback ${type}`, `<p>Comentário: ${comment}</p>`].join(
-      "\n"
-    ),
-  });
-
-  return response.status(201).send();
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("HTTP server running");
